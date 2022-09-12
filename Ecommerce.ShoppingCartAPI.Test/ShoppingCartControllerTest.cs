d7fa0e5a55ac6e8ceb3a6c51c581235113952527
mohamed <mohamedzaghlol74111@gmail.com>
2022-09-13 00:25:16 +0200

    add AddItemToNonExistingShoppingCart, AddItemToExistingShoppingCart test cases

--- Ecommerce.ShoppingCartAPI.Test/ShoppingCartControllerTest.cs
+++ Ecommerce.ShoppingCartAPI.Test/ShoppingCartControllerTest.cs
@@ -1,16 +1,35 @@
+using Ecommerce.ShoppingCartAPI.Controllers;
+using Ecommerce.ShoppingCartAPI.Repositories;
+using FakeItEasy;
 using NUnit.Framework;
 
 namespace Ecommerce.ShoppingCartAPI.Test
 {
     public class ShoppingCartControllerTest
     {
+        ShoppingCartController controller = null;
+
         [SetUp]
         public void Setup()
         {
+            var shoppingCartRepository = A.Fake<IShoppingCartRepository>();
+            controller = new ShoppingCartController(shoppingCartRepository);
         }
 
         [Test]
         public void AddItemToNonExistingShoppingCart()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AddItemToExistingShoppingCart()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AddExistingItemToExistingShoppingCart()
         {
             Assert.Pass();
         }
